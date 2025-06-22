789b26c811fc13d63856367e3484fcb4e8c338e2
Tharushika Dilakshan <101235785+TharuZika@users.noreply.github.com>
2025-06-22 21:07:44 +0530

    create backend

---
+++ src/index.ts
@@ -0,0 +1,28 @@
+// src/index.ts
+import express, { Application, RequestHandler } from 'express'
+import cors from 'cors'
+
+const app: Application = express()
+app.use(cors())
+app.use(express.json())
+
+// routes
+
+const homeHandler: RequestHandler = (req, res) => {
+  res.send('SympCheck Backend is up!')
+}
+
+const apiv1Handler: RequestHandler = (req, res) => {
+    res.send('API v1 is Ready!')
+  }
+
+app.get('/', homeHandler)
+app.get('/api/v1', apiv1Handler)
+
+app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
+  console.error(err.stack)
+  res.status(500).send('Server Error')
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, () => console.log(`Server running on ${PORT}`))
